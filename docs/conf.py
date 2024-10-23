0900ba94c2ce6b6344cc1ed3996885af26db8889
Dmitry Zankov <dvzankov@gmail.com>
2024-10-23 11:09:09 +0900

    jupyter tutorials updated

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # General configuration
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'nbsphinx', 'nbsphinx_link']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'nbsphinx']
 source_suffix = '.rst'
 master_doc = 'index'
 
