ea04699c4cd4ee86e9990ae971c2d5e92f6d8921
Almaz GILMULLIN <agilmullin@calixarene.chimie.unistra.fr>
2026-06-05 10:18:46 +0200

    added in init file the mhn_tuning

--- synplan/ml/training/__init__.py
+++ synplan/ml/training/__init__.py
@@ -1,11 +1,16 @@
 from .preprocessing import MENDEL_INFO, ValueNetworkDataset, mol_to_pyg
 from .supervised import *
-from .supervised import create_policy_dataset, run_policy_training
+from .supervised import (
+    create_policy_dataset,
+    run_mhn_network_tuning,
+    run_policy_training,
+)
 
 __all__ = [
     "MENDEL_INFO",
     "ValueNetworkDataset",
     "create_policy_dataset",
     "mol_to_pyg",
+    "run_mhn_network_tuning",
     "run_policy_training",
 ]
